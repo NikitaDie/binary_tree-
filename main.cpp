1fad97090fd82335849ac122efba4d8224d8757a
NikitaDie <niki0609tak@gmail.com>
2023-02-18 00:43:12 +0100

    v1.5 ("remove" works partly)

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ enum {
     s_left = 2
 };
 
-//template <class T, class K>
+template <class T>
 class Node
 {
 private:
@@ -15,10 +15,9 @@ private:
     Node* left;
     Node* prev;
     int side;
-    //K key;
 
 public:
-    Node(int value) :
+    Node(T value) :
         value{ value },
         right{ nullptr },
         left{ nullptr },
@@ -38,18 +37,35 @@ public:
         this->value = new_value;
     }
 
-    void setRight(Node* new_right)
+    void setRight(Node<T>* new_right)
     {
+        if (new_right == nullptr)
+        {
+            this->right = nullptr;
+            return;
+        }
+
         this->right = new_right;
         this->right->prev = this;
         new_right->side = s_right;
     }
 
-    void setLeft(Node* new_left)
+    void setLeft(Node<T>* new_left)
     {
+        if (new_left == nullptr)
+        {
+            this->left = nullptr;
+            return;
+        }
+
         this->left = new_left;
         this->left->prev = this;
         new_left->side = s_left;
+    }
+
+    void setPrev(Node<T>* new_prev)
+    {
+        this->prev = new_prev;
     }
 
     const int& getValue() const
@@ -62,7 +78,17 @@ public:
         return this->right;
     };
 
+    Node* getRight() const
+    {
+        return this->right;
+    };
+
     Node* getLeft()
+    {
+        return this->left;
+    }
+
+    Node* getLeft() const
     {
         return this->left;
     }
@@ -72,12 +98,13 @@ public:
         return this->prev;
     };
 
-    int getside() const
+    int getSide() const
     {
         return this->side;
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const Node& node);
+    template <class C>
+    friend std::ostream& operator<<(std::ostream& os, const Node<C>& node);
 
     ~Node()
     {
@@ -92,17 +119,19 @@ public:
 
 };
 
-std::ostream& operator<<(std::ostream& os, const Node& node)
+template <class C>
+std::ostream& operator<<(std::ostream& os, const Node<C>& node)
 {
     return os << node.getValue();
 }
 
+template <class T>
 class Tree
 {
 private:
-    Node* root;
+    Node<T>* root;
 
-    Node* chooseNextPosition(Node* current, int value) const
+    Node<T>* chooseNextPosition(Node<T>* current, int value) const
     {
 
         if (current == nullptr)
@@ -119,12 +148,20 @@ private:
         return current;
     }
 
+    bool isLeaf(const Node<T>* current) const
+    {
+        if (current->getLeft() == nullptr && current->getRight() == nullptr)
+            return true;
+
+        return false;
+    }
+
 public:
     Tree() :
         root{ nullptr }
     {}
 
-    Node* getRoot()
+    Node<T>* getRoot()
     {
         return this->root;
     }
@@ -134,12 +171,12 @@ public:
 
         if (this->root == nullptr)
         {
-            this->root = new Node(value);
+            this->root = new Node<T>(value);
             return;
         }
 
-        Node* current{ nullptr };
-        Node* next_current{ this->root };
+        Node<T>* current{ nullptr };
+        Node<T>* next_current{ this->root };
 
         while (next_current != nullptr)
         {
@@ -149,15 +186,65 @@ public:
 
         if (value >= current->getValue())
         {
-            current->setRight(new Node(value));
+            current->setRight(new Node<T>(value));
         }
         else
         {
-            current->setLeft(new Node(value));
+            current->setLeft(new Node<T>(value));
         }
     }
 
-    const Node* getPrev(Node* current) const
+    void remove(T val)
+    {
+        Node<T>* current{ this->find(val, this->root) };
+
+        if (current == nullptr)
+        {
+            std::cout << "element doesn`t exist" << std::endl;
+            return;
+        }
+
+        if (isLeaf(current))
+        {
+            delete current;
+            return;
+        }
+
+        Node<T>* tmp{ this->getNext(current) };
+
+        if (current->getRight() != nullptr && current->getLeft() != nullptr)
+        {
+
+            if (tmp->getSide() == s_right)
+                tmp->getPrev()->setRight(nullptr);
+            else
+                tmp->getPrev()->setLeft(nullptr);
+
+            tmp->setRight(current->getRight());
+            tmp->setLeft(current->getLeft());
+
+        }
+
+        if (current->getSide() == s_right)
+        {
+            current->getPrev()->setRight(tmp);
+        } 
+        else if (current->getSide() == s_left)
+        {
+            current->getPrev()->setLeft(tmp);
+        }
+       
+        tmp->setPrev(current->getPrev());
+
+        current->setRight(nullptr);
+        current->setLeft(nullptr);
+
+        delete current;
+        return;
+    }
+
+
+    Node<T>* getPrev(Node<T>* current)
     {
         if (current == nullptr)
             return nullptr;   //TODO throw exception
@@ -193,7 +280,7 @@ public:
         return nullptr;
     }
 
-    const Node* getNext(Node* current) const
+    Node<T>* getNext(Node<T>* current) 
     {
         if (current == nullptr)
             return nullptr;   //TODO throw exception
@@ -210,7 +297,7 @@ public:
             return current;
         }
 
-        if (current->getside() == s_left)
+        if (current->getSide() == s_left)
         {
             return current->getPrev();
         }
@@ -229,9 +316,9 @@ public:
         return nullptr;
     }
 
-    Node* find(int value, Node* root) 
+    Node<T>* find(int value, Node<T>* root) 
     {
-        Node* current{ root };
+        Node<T>* current{ root };
 
         if (root == nullptr)
         {
@@ -246,7 +333,7 @@ public:
         find(value, current);
     }
 
-    void printTree(Node* root)
+    void printTree(Node<T>* root)
     {
 
         if (root != nullptr)
@@ -256,12 +343,6 @@ public:
             printTree(root->getRight());
         }
 
-    }
-
-    void dl()
-    {
-        if (root != nullptr)
-            delete(this->root);
     }
 
     ~Tree()
@@ -275,9 +356,9 @@ public:
 int main()
 {
 
-    Tree tree;
+    Tree<int> tree;
 
-    tree.insert(45);
+    /*tree.insert(45);
     tree.insert(55);
     tree.insert(34);
     tree.insert(28);
@@ -289,8 +370,27 @@ int main()
 
     std::cout << std::endl;
 
-    //Node* tmp = tree.find(1, tree.getRoot());
+    //Node<T>* tmp = tree.find(1, tree.getRoot());
 
     //std::cout << tree.getPrev(tree.find(55, tree.getRoot()))->getValue() << std::endl;
-    std::cout << tree.getNext(tree.find(32, tree.getRoot()))->getValue() << std::endl;
+    std::cout << tree.getNext(tree.find(32, tree.getRoot()))->getValue() << std::endl;*/
+
+    tree.insert(89);
+    tree.insert(92);
+    tree.insert(90);
+    tree.insert(28);
+    tree.insert(58);
+    tree.insert(32);
+    tree.insert(37);
+    tree.insert(79);
+    tree.insert(72);
+    tree.insert(23);
+    tree.insert(2);
+    tree.insert(10);
+
+    tree.printTree(tree.getRoot());
+    std::cout << std::endl;
+
+    tree.remove(58);
+    tree.printTree(tree.getRoot());
 }
